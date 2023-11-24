cc20d1f70b766f2f052a8d18afad862dac2bcada
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-11-24 11:30:06 +0100

    Olympiaden 3

---
+++ Olympiaden, fråga 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("n?");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("m?");
+        int m = int.Parse(Console.ReadLine());
+
+        char[,] grid = new char[n, m];
+
+        for (int i = 0; i <n; i++)
+        {
+            Console.WriteLine($"Rad {i + 1} ?");
+            string row = Console.ReadLine();
+
+            for (int j = 0; j < m; j++)
+            {
+                grid[i, j] = row[j];
+            }
+        }
+        Console.Write("svar:");
+        TraverseGrid(grid, n, m);
+    }
+
+    static void TraverseGrid(char[,] grid, int n, int m)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0;j < m; j++)
+            {
+                if (i + j == n - 1)
+                {
+                    Console.Write(grid[i, j]);
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+}
