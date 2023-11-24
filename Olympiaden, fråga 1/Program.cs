eefe0c5747c29344f5e419c3039eadd113968bf0
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-11-24 09:45:13 +0100

    Olympiaden
    fråga1

---
+++ Olympiaden, fråga 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Skriv in ett ord? ");
+        string ord = Console.ReadLine();
+
+        if (ord.Length > 9)
+        {
+            Console.WriteLine("Ordet får inte vara längre än 9 bokstäver.");
+            return;
+        }
+
+        Console.Write("Hur många upprepningar vill du ha? ");
+        int antalUpprepningar = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < antalUpprepningar; i++)
+        {
+            Console.Write(ord);
+        }
+    }
+}
